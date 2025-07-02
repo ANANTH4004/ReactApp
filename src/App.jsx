c7b4ec6a4a5587c4b55b31165290757b8676513a
ANANTH4004 <balamuruganananth@gmail.com>
2025-07-02 08:21:31 +0530

    States in class components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-import ClassCompoonents from './components/ClassComponent'
+import ClassComponents from './components/ClassComponent'
 import Student from './components/Student'
 import EventsExample from './components/EventsExample'
 
 function App() {
   return (
     <>
-      <EventsExample />
+      <ClassComponents />
     </>
   )
 }
--- src/components/ClassComponent.jsx
+++ src/components/ClassComponent.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 
-class ClassCompoonents extends React.Component {
+class ClassComponents extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      name: 'Anand',
+      count: 0,
+    }
+  }
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    })
+    console.log(this.state)
+  }
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    })
+  }
   render() {
     return (
       <>
+        <h1>Count is {this.state.count}</h1>
         <h1> This is a class component </h1>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
       </>
     )
   }
 }
 
-export default ClassCompoonents
+export default ClassComponents
