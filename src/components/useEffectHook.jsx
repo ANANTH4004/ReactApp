a4c293ec9f9322889e080ab28264dd93da232b79
ANANTH4004 <balamuruganananth@gmail.com>
2025-07-04 08:48:33 +0530

    use effectHook

--- src/components/useEffectHook.jsx
+++ src/components/useEffectHook.jsx
@@ -1,7 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
+import '../styles/hooks.scss'
 const UseEffectHook = () => {
-  console.log('Component rendered')
   const [users, setUsers] = useState([])
+  const name = useRef(null)
+  const phone = useRef(null)
+  const username = useRef(null)
+  const website = useRef(null)
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users').then((response) => {
       response.json().then((data) => {
@@ -10,12 +14,51 @@ const UseEffectHook = () => {
       })
     })
   }, [])
+
+  const addNewUser = () => {
+    const newUser = {
+      id: users.length + 1,
+      name: name.current.value,
+      phone: phone.current.value,
+      username: username.current.value,
+      website: website.current.value,
+    }
+    setUsers([...users, newUser])
+    name.current.value = ''
+    phone.current.value = ''
+    username.current.value = ''
+    website.current.value = ''
+  }
+
   return (
     <>
       <h1>useEffect Hook</h1>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <div className="form">
+        <div style={{ gap: '20px' }}>
+          <label htmlFor="name">Name:</label>
+          <input ref={name} type="text" id="name" name="name" />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone:</label>
+          <input ref={phone} type="text" id="phone" name="phone" />
+        </div>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input ref={username} type="text" id="username" name="username" />
+        </div>
+        <div>
+          <label htmlFor="website">Website:</label>
+          <input ref={website} type="text" id="website" name="website" />
+        </div>
+        <div>
+          <button onClick={addNewUser}> Add New User</button>
+        </div>
+      </div>
     </>
   )
 }
