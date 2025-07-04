f058cf77481a8e9931b4f4478c5a194f3257dbe0
ANANTH4004 <balamuruganananth@gmail.com>
2025-07-04 16:39:54 +0530

    child to parent button calls

--- src/components/parent-child/Child.jsx
+++ src/components/parent-child/Child.jsx
@@ -1,12 +1,20 @@
 
-
-const Child = ({content}) => {
+import { useEffect } from "react"
+const Child = (props) => {
+    const childFn = () => {
+        console.log("Child function called")
+    }
+    useEffect(()=> {
+        childFn()
+    }, [props.callChild])
     return (
        <>
        <h1>This is a Child Component</h1>
+       <input type="text" onChange={(e) => props.setParent(e.target.value)} />
        <div style={{display: 'flex',justifyContent: 'center', flexDirection:'row'}}>
-        <p>Content from parent component: {content} </p>
+        <p>Content from parent component: {props.content} </p>
        </div>
+       <button onClick={()=> props.parentFn()}>Call parent function</button>
        </>
     )
 }
--- src/components/parent-child/Parent.jsx
+++ src/components/parent-child/Parent.jsx
@@ -2,12 +2,24 @@ import { useState } from "react"
 import Child from "./Child"
 
 const Parent = () => {
+
+   const [callChild, setCallChild] = useState(false);
+   const parentFn = () => {
+      console.log("parent function called");
+   }
+   const callParent = () => {
+      setCallChild(true)
+   }
+    const [fromChild, setFromChild] = useState('')
     const [content, setContent] = useState("empty content")
      return (
         <>
         <h1>This is a Parent Component</h1>
+        <p>this is the content from child: {fromChild}</p>
+        <button onClick={() => callParent()}>CallChild Fn</button>
+
         <input type="text" onChange={(e) => setContent(e.target.value) }/>
-        <Child content={content}></Child>
+        <Child content={content} setParent={setFromChild} parentFn={parentFn} callChild={callChild} />
         </>
      )
 }
