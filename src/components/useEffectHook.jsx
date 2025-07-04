41275ba400523dadce128d0428533c07103d3c95
ANANTH4004 <balamuruganananth@gmail.com>
2025-07-04 13:27:26 +0530

    changes to work react app in stackblitz

--- src/components/useEffectHook.jsx
+++ src/components/useEffectHook.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useRef } from 'react'
-import '../styles/hooks.scss'
+import styles from '../styles/hooks.module.css'
 const UseEffectHook = () => {
   const [users, setUsers] = useState([])
   const name = useRef(null)
@@ -7,7 +7,14 @@ const UseEffectHook = () => {
   const username = useRef(null)
   const website = useRef(null)
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users').then((response) => {
+    fetch('https://jsonplaceholder.typicode.com/users',{
+      method: 'GET',
+      mode: 'cors', // enable CORS (default for cross-origin fetch)
+      headers: {
+        'Accept': 'application/json',
+        'X-Custom-Header': 'my-custom-header-value'
+      }
+    }).then((response) => {
       response.json().then((data) => {
         console.log('inside use effect', data)
         setUsers(data)
@@ -38,7 +45,7 @@ const UseEffectHook = () => {
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
-      <div className="form">
+      <div className={styles.form}>
         <div style={{ gap: '20px' }}>
           <label htmlFor="name">Name:</label>
           <input ref={name} type="text" id="name" name="name" />
